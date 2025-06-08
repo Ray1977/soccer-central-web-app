9280ba4cc48b19c78ef12e446c1d429fd4ce86b4
Jose Espinoza <espinozajgx@gmail.com>
2025-06-08 17:04:09 +0200

    actualizacion de interfaz

--- pages/player_report_from_layout.py
+++ pages/player_report_from_layout.py
@@ -17,10 +17,18 @@ def connect_to_db():
         st.error(e)
         return None
 =======
+import plotly.graph_objects as go
 from sqlalchemy.orm import Session
 from models import Users, Players
 from db import engine  # Asegúrate de tener tu engine SQLAlchemy configurado
 >>>>>>> 16ef97c (Actualizar conector BD a SQLAlchemy, modificar boton de descarga de reporte)
+import plotly.graph_objects as go
+import random
+
+login.generarLogin()
+
+if "usuario" not in st.session_state:
+    st.stop()
 
 def calculate_age(birth_date):
     today = datetime.today()
@@ -32,7 +40,24 @@ def calculate_goals_per_90(goals, minutes_played):
 def radar_chart():
     categories = ['On Ball', 'Intelligence', 'Shot', 'Defensive', 'Aerial', 'Physical']
     values = [random.randint(60, 95) for _ in categories]
-    fig = px.line_polar(r=values, theta=categories, line_close=True, template="plotly_dark")
+    # Cerramos el radar uniendo el primer punto
+    values.append(values[0])
+    categories.append(categories[0])
+
+    fig = go.Figure(
+        data=[
+            go.Scatterpolar(
+                r=values,
+                theta=categories,
+                fill='toself',
+                name='Player Performance',
+                line_color='lime'
+            )
+        ]
+    )
+
+    
+
     return fig
 
 <<<<<<< HEAD
